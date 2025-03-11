d7aa95893bea2f6095abcfe70cf60c7574d32480
awaisjbr <awaisjbr@gmail.com>
2025-03-11 16:33:27 +0500

    11-03-2025

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import omega from "../assets/omega/omega.jpg";
 import grandcity2 from "../assets/grandcity/grandcity.png";
+import grandcity5 from "../assets/grandcity/grandcity5.png";
+import sitara from "../assets/sitara/sitaracard.jpg";
+
+const data = [{img:omega},{img: grandcity5},{img:sitara}]
 
 
 const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide((prev) => prev === data.length -1 ? 0 : prev +1)
+    },10000);
+      
+    return () => clearInterval(slideInterval)
+  })
+
   return (
     <motion.div initial={{opacity: 0.1}} animate={{opacity:1}} transition={{ duration: 2, ease:"easeIn" }} className='mt-10 sm:mt-24 md:mt-3 lg:mt-24 z-10 flex flex-col justify-center w-full min-h-screen' style={{zIndex: -1}} id='home'>
       {/* //Hero Image// */}
-      <motion.img initial={{opacity: 0.1, y: -50}} animate={{opacity:1, y: 0}} transition={{ duration: 1, ease:"easeOut", delay: 0.5 }} src={omega} className='' alt="" />
+      <div className='relative w-full whitespace-nowrap overflow-hidden carousel'>
+      {data.map((item,index) => {
+        return <motion.img initial={{opacity: 0.1}} animate={{opacity:1}} transition={{ duration: 1, ease:"easeOut" }} src={item.img} key={index} className={`${currentSlide === index ? "" : "hidden"} w-full flex flex-shrink-0`} alt="" />
+      })}
+      </div>
+
+      
       <motion.img initial={{opacity: 0, y: 50}} animate={{opacity:1, y: 0}} transition={{ duration: 1, ease:"easeOut", delay: 1.5 }} src={grandcity2} className='md:hidden mt-10' alt="" />
     
         {/* Hero Text  */}
-      <motion.h1 initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeOut", delay:3 }}
-      className='absolute self-center bg-black/80 md:text-2xl lg:text-5xl text-white p-2 hidden md:block'>FIND YOUR DREAM PLACE TO LIVE</motion.h1>
+      {/* <motion.h1 initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeOut", delay:3 }}
+      className='absolute self-center bg-black/80 md:text-2xl lg:text-5xl text-white p-2 hidden md:block'>FIND YOUR DREAM PLACE TO LIVE</motion.h1> */}
     </motion.div>
   )
 }
--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -5,7 +5,6 @@ import grandcity1 from "../assets/grandcity/grandcity-hero.jpeg";
 import grandcity2 from "../assets/grandcity/grandcity1.jpeg";
 import grandcity3 from "../assets/grandcity/grandcity2.jpeg";
 import grandcity4 from "../assets/grandcity/grandcity3.jpeg";
-import grandcity5 from "../assets/grandcity/grandcity5.png";
 import cart from "../assets/icons/cart.png";
 import cctv from "../assets/icons/cctv.png";
 import cinema from "../assets/icons/cinema.png";
@@ -15,11 +14,31 @@ import park from "../assets/icons/park.png";
 import restaurant from "../assets/icons/restaurant.png";
 import saloon from "../assets/icons/saloon.png";
 import school from "../assets/icons/school.png";
+import video from "../assets/grandcity/grandvideoceo.mp4";
+import amenities from "../assets/grandcity/amenities.jpeg";
+import amenities1 from "../assets/grandcity/amenities1.jpeg";
+import amenities2 from "../assets/grandcity/amenities2.jpeg";
+import amenities3 from "../assets/grandcity/amenities3.jpeg";
 
+// const data = [{img: grandcity},{img: grandcity1},{img:grandcity2},{img:grandcity4},{img:grandcity3}]
 
 const Property = () => {
   const location = useLocation();
   const { name } = useParams();
+  const [imgSrc, setImgSrc] = useState(null)
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        setImgSrc(null);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <div className="pt-24 lg:pt-28 flex flex-col items-center justify-center">
@@ -29,13 +48,13 @@ const Property = () => {
         <p className="text-xs md:text-sm lg:text-lg font-semibold mt-1">Sarghodha Road Faisalabad.</p>
         <div className="flex flex-col lg:flex-row items-center gap-5 justify-evenly mt-5 mb-5 px-5 w-full">
           <div>
-          <img src={grandcity} className="w-[450px] md:w-[500px] cursor-pointer"/>
+          <img src={grandcity} className="w-[450px] md:w-[500px] cursor-pointer" onClick={() => setImgSrc(grandcity)}/>
           </div>
           <div className="grid grid-cols-4 lg:grid-cols-2 gap-5">
-            <img src={grandcity1} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" />
-            <img src={grandcity2} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" />
-            <img src={grandcity4} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" />
-            <img src={grandcity3} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" />
+            <img src={grandcity1} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" onClick={() => setImgSrc(grandcity1)} />
+            <img src={grandcity2} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" onClick={() => setImgSrc(grandcity2)} />
+            <img src={grandcity4} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" onClick={() => setImgSrc(grandcity4)} />
+            <img src={grandcity3} className="w-[151px] lg:w-[237px] cursor-pointer" alt="" onClick={() => setImgSrc(grandcity3)} />
           </div>
         </div>
         {/* Sizes Available */}
@@ -50,7 +69,11 @@ const Property = () => {
           </div>
         </div>
       </div>
-
+      {imgSrc && (
+        <div className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 p-5" onClick={() => setImgSrc(null)}>
+          <img src={imgSrc} alt="full screen" className="max-w-full max-h-full rounded-lg shadow-lg" />
+        </div>
+      )}
       
     {/* Amenities */}
       <div className="bg-gray-100 mt-10 w-full flex flex-col items-center">
@@ -101,12 +124,46 @@ const Property = () => {
             <h1 className="text-lg font-semibold underline underline-offset-2">PARLOUR</h1>
             <p className="w-[90%] mx-auto text-center">Opera Cinema is your destination for cinematic excellence and immersive entertainment.</p>
           </div>
-          
+        </div>
+      {/* visual amenities */}
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 pb-5 gap-10 lg:gap-7">
+          <img src={amenities} className="w-[300px]  lg:w-[350px] cursor-pointer" alt="" onClick={() => setImgSrc(amenities)} />
+          <img src={amenities1} className="w-[300px] lg:w-[350px] cursor-pointer" alt="" onClick={() => setImgSrc(amenities1)} />
+          <img src={amenities2} className="w-[300px] lg:w-[350px] cursor-pointer" alt="" onClick={() => setImgSrc(amenities2)} />
+          <img src={amenities3} className="w-[300px] lg:w-[350px] cursor-pointer" alt="" onClick={() => setImgSrc(amenities3)} />
         </div>
       </div>
+
+
+      {/* Our Message */}
+        <div className="my-5 lg:my-10 w-full">
+          <h1 className="font-bold text-center text-xl lg:text-3xl py-2 px-3 rounded-md my-5">Our Message</h1>
+          <div className="overflow-hidden flex gap-2 flex-col lg:flex-row">
+            <div className="flex-1 bg-red-50 flex items-center justify-center">
+              <video width={250} controls >
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div className="flex-1 flex items-center justify-center px-20">
+              <div className="hidden lg:block">
+                <p>At Raees Estate, we are dedicated to helping you find the perfect place to call home. With years of experience in the real estate market, our team of expert agents is committed to providing personalized service, market insights, and seamless transactions.
+                <br /><br />Whether you're buying, selling, or investing, we offer a full range of real estate solutions tailored to your needs.
+                <br /><br /> Our deep understanding of local markets, cutting-edge technology, and client-first approach set us apart.We believe that finding the right property is more than just a transaction—it’s about creating a lifestyle.
+                <br /><br /> Let us guide you every step of the way and turn your real estate dreams into reality.
+                <br /><br />Let’s find your dream home together!</p>
+               </div>
+              </div>
+          </div>
+        </div>
 
     </div>
   );
 };
 
 export default Property;
+
+
+{/* <div className="grid grid-cols-4 lg:grid-cols-2 gap-5">
+
+</div> */}
